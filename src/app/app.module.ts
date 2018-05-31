39517e8d88c65123162dcd6594f44fcf8bbf6d21
Lapo Pancani <stelapo@gmail.com>
2018-05-31 09:19:35 +0200

    Managed env vars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthenticationGuard, MsAdalAngular6Module } from 'microsoft-adal-angular6';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { DivisionComponent } from './division/division.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -24,12 +23,10 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     AppRoutingModule,
     HttpClientModule,
     MsAdalAngular6Module.forRoot({
-      tenant: '<tenant>',
-      clientId: '<clientId>',
+      tenant: environment.tenant,
+      clientId: environment.clientId,
       redirectUri: window.location.href,
-      endpoints: {
-        '<url>': '<id>'
-      },
+      endpoints: environment.endpoints,
       navigateToLoginRequestUrl: false,
       cacheLocation: 'sessionStorage',
     })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+import { env } from './env';
+
 export const environment = {
-  production: true
+  production: true,
+  tenant: env.tenant,
+  clientId: env.clientId,
+  endpoints: env.endpoints
 };
